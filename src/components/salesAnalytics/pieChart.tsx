f451569f453359cd27e266c3747c8c61d6c3bf6d
kaviyarasan <kaviyarasan@way2smile.com>
2024-04-12 10:43:22 +0530

    pie chart update

--- src/components/salesAnalytics/pieChart.tsx
+++ src/components/salesAnalytics/pieChart.tsx
@@ -7,23 +7,22 @@ interface Props {
 
 const PieChart = ({ data }: Props) => {
     const config = {
-        data: data,
+        data,
         angleField: 'value',
         colorField: 'type',
+        radius: 0.8,
         label: {
-            text: 'value',
-            style: {
-                fontWeight: 'bold',
-            },
+          text: (d:any) => `${d.type}\n ${d.value}`,
+          position: 'spider',
         },
         legend: {
-            color: {
-                title: false,
-                position: 'right',
-                rowPadding: 5,
-            },
+          color: {
+            title: false,
+            position: 'right',
+            rowPadding: 5,
+          },
         },
-    };
+      };
     return <Pie {...config} />;
 };
 
